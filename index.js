816375059ff98cd936117d033c0368e56fb2c265
rsalasv <riicky.salas@gmail.com>
2021-03-18 14:06:20 -0600

    Aún tiene las credenciales en el código pero ya jaló el server

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const express = require('express')
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
+const cors = require('cors')
+const { json, urlencoded } = express
+
+const PORT = 3000
+const HOST = "0.0.0.0"
+
+const app = express()
+
+app.use(json())
+app.use(urlencoded({ extended: false }))
+const corsOptions = { origin: '*', optionsSuccessStatus: 200 }
+app.use(cors(corsOptions))
+
+app.get('/', function (req, res) {
+	res.send("jalando");
+});
 
 const toneAnalyzer = new ToneAnalyzerV3({
   version: '2017-09-21',
@@ -9,7 +26,24 @@ const toneAnalyzer = new ToneAnalyzerV3({
   serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/8b56da32-908d-48dc-ace0-aee45288a1fe',
 });
 
-toneAnalyzer.method(params)
-  .catch(err => {
-    console.log('error:', err);
-  });
+app.post('/ricardo-salas', function (req, res) {
+
+    const text = req.body.text;
+  
+    const toneParams = {
+      toneInput: { 'text': text },
+      contentType: 'application/json',
+    };
+  
+    toneAnalyzer.tone(toneParams)
+      .then(toneAnalysis => {
+        console.log(JSON.stringify(toneAnalysis, null, 2));
+        res.send(toneAnalysis.result);
+      })
+      .catch(err => {
+        console.log('error:', err);
+        res.send(err);
+      });	
+});
+
+app.listen(PORT,HOST, () => { console.log(`Server listening on port ${PORT}`); })
